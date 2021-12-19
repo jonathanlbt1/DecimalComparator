ca81253df6ab29af893f9b3b60313dcc5030f35c
Jonathan Moraes <jonathan.pmoraes@senacsp.edu.br>
2021-12-18 22:30:47 -0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args){
+        boolean isEqual = DecimalComparator.areEqualByThreeDecimalPlaces(-3.1756, -3.175);
+        System.out.println(isEqual);
+    }
+}
